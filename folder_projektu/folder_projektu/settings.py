ce756782a2ec6326b7234cfbd13d1b9d66f57fd0
zvzeq-swps <zbednarek@st.swps.edu.pl>
2024-11-28 16:57:11 +0100

    zapis

--- folder_projektu/folder_projektu/settings.py
+++ folder_projektu/folder_projektu/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'folder_aplikacji.apps.FolderAplikacjiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
