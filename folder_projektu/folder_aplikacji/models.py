0fa0b83e204c3a2b1c9990058de82769ad0f685a
zvzeq-swps <zbednarek@st.swps.edu.pl>
2024-12-03 17:19:22 +0100

    nowe modele

--- folder_projektu/folder_aplikacji/models.py
+++ folder_projektu/folder_aplikacji/models.py
@@ -30,3 +30,28 @@ class Person(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Osoba(models.Model):
+    PLEC_CHOICES = (
+        ("K", "Kobieta"),
+        ("M", "Męzczyzna"),
+        ("I", "Inna"),
+    )
+
+    imie = models.CharField(max_length=40, blank=False, null=False)
+    nazwisko = models.CharField(max_length=60, blank=False, null=False)
+    plec = models.CharField(max_length=1, choices=PLEC_CHOICES, default=PLEC_CHOICES[2][0])
+    stanowisko = models.ForeignKey("Stanowisko", on_delete = models.CASCADE)
+
+    def __str__(self):
+        return f"{self.imie} {self.nazwisko}"
+
+
+class Stanowisko(models.Model):
+    nazwa = models.CharField(max_length=80, blank=False, null=False)
+    opis = models.TextField(blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+    
